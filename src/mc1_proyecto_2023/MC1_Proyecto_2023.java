7a40411995b5a62ed726c1068fc5648c79f31701
202208521-luis-morales <3683798410101@ingenieria.usac.edu.gt>
2023-04-23 18:45:49 -0600

    First commit

---
+++ src/mc1_proyecto_2023/MC1_Proyecto_2023.java
@@ -0,0 +1,12 @@
+package mc1_proyecto_2023;
+
+public class MC1_Proyecto_2023 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
